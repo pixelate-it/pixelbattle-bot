7dcc26dc42b33a1c4f497f2c1188a1cd71315c28
mirdukkkkk <69540930+mirdukkkkk@users.noreply.github.com>
2026-03-04 22:26:19 +0300

    revert "feat: anti user-installed app"

--- src/listeners/messages/MessageListener.js
+++ src/listeners/messages/MessageListener.js
@@ -1,6 +1,5 @@
 const PixelListener = require('../../structures/PixelListener');
 const CommandsExecutorService = require('../../services/CommandExecutorService');
-const AntiUserappService = require('../../services/AntiUserappService');
 
 class MessageListener extends PixelListener {
     constructor() {
@@ -9,10 +8,6 @@ class MessageListener extends PixelListener {
 
     async run(client, message) {
         if(!message.guild) return;
-
-        const antiUserapp = new AntiUserappService(client);
-        if(!await antiUserapp.checkMessage(message)) return;
-
         if(message.author.bot) return;
 
         const executor = new CommandsExecutorService(message, client);
